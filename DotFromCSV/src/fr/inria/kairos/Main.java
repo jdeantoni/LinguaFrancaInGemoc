733ed409f1543122e601b201f7d10fef85ee1af8
jmpcambeiro <jmc12976@campus.fct.unl.pt>
2021-06-11 10:43:46 +0100

    Added new graph representation indicating the executed reactions produced effects.

--- DotFromCSV/src/fr/inria/kairos/Main.java
+++ DotFromCSV/src/fr/inria/kairos/Main.java
@@ -80,7 +80,7 @@ public class Main {
                 writer.newLine();
                 if(currentTransition.getPreviousTransition() != null)
                     sb.append(nodeId - 1).append(" -> ").append(nodeId).append(" [label=\"")
-                            .append(generateLabel(currentTransition.getPreviousStepReactions())).append("\"]\n");
+                            .append(generateLabel(currentTransition.getTriggers())).append("\"]\n");
                 currentTransition = currentTransition.getNextTransition();
                 nodeId++;
             }
@@ -115,20 +115,23 @@ public class Main {
         Transition rootTransition = new Transition(0,0, null, null,
                 null, false);   //graph root node
         Transition currentTransition = rootTransition;
-        Set<String> pendingEffects = new HashSet<>();
+        //Set<String> pendingEffects = new HashSet<>();
         List<String> pendingLabels = new LinkedList<>();
+        List<String> effects = new LinkedList<>();
         for (TraceRecord record: traceRecords) {
             if(!record.getEvent().startsWith(REACTION) && !record.getEvent().startsWith(WORKER_ENDS))
                 continue;
             if(record.getElapsedLogicalTime() > nextLogicalTimeAdvance || record.getMicroStep() > currentMicroStep) {
                 if(pendingLabels.size() > 0) {
-                    currentTransition = addTransition(currentTransition, pendingLabels, nextLogicalTimeAdvance,
-                            currentMicroStep, false);
+                    currentTransition = addReactionEffectsTransitions(currentTransition, effects, pendingLabels,
+                            nextLogicalTimeAdvance, currentMicroStep, false);
                     pendingLabels = new LinkedList<>();
+                    if(effects.size() > 0)
+                        effects = new LinkedList<>();
                 }
                 nextLogicalTimeAdvance = record.getElapsedLogicalTime();
                 currentMicroStep = record.getMicroStep();
-                pendingEffects.clear();
+                //pendingEffects.clear();
                 currentTransition = addTransition(currentTransition, pendingLabels, nextLogicalTimeAdvance,
                         currentMicroStep, true);
                 pendingLabels = new LinkedList<>();
@@ -136,18 +139,18 @@ public class Main {
             if(record.getEvent().equals(REACTION_STARTS)){
                 if(record.getElapsedLogicalTime() == nextLogicalTimeAdvance && currentMicroStep ==
                         record.getMicroStep()){
-                    if (triggersPrecededByEffects(record.getTriggeredBy(), pendingEffects)) {  // causality from previous reaction on same Logical time
-                        currentTransition = addTransition(currentTransition, pendingLabels, nextLogicalTimeAdvance,
-                                currentMicroStep, false);
-                        if (pendingEffects.size() > 0)
-                            currentTransition.setPendingEffects(pendingEffects);
+                    if (triggersPrecededByEffects(record.getTriggeredBy(), effects)) {  // causality from previous reaction on same Logical time
+
+                        currentTransition = addReactionEffectsTransitions(currentTransition, effects, pendingLabels,
+                                nextLogicalTimeAdvance, currentMicroStep, false);
                         pendingLabels = new LinkedList<>();
+                        effects = new LinkedList<>();
                     }
                     pendingLabels.add(generateLinkLabel(record));
                 }
             }else if(record.getEvent().equals(REACTION_ENDS))
                 if(record.getEffects() != null)
-                    addReactionEffects(record.getEffects(), pendingEffects);
+                    addEffects(record.getEffects(), effects);
         }
         return rootTransition;
     }
@@ -169,14 +172,13 @@ public class Main {
         currentTransition.setNextTransition(nextTransition);
         nextTransition.setPreviousTransition(currentTransition);
         if(resultsFromWorkerAdvancing) {
-            nextTransition.setPreviousStepReactions(new LinkedList<>());
-            nextTransition.getPreviousStepReactions().add(TIME_ADVANCE_LABEL_PREFIX);
+            nextTransition.setTriggers(new LinkedList<>());
+            nextTransition.getTriggers().add(TIME_ADVANCE_LABEL_PREFIX);
         }
         else
-            nextTransition.setPreviousStepReactions(pendingReactions);
+            nextTransition.setTriggers(pendingReactions);
         return nextTransition;
     }
-
 
     /**
      *
@@ -185,22 +187,21 @@ public class Main {
      * @return a boolean indicating if at least one of a reactionºs triggers is contained on the set of pending effects.
      * A true return value indicates a causality between a reaction and a previous executed reaction.
      */
-    private static boolean triggersPrecededByEffects(String[] reactionTriggers, Set<String> pendingEffects) {
+    private static boolean triggersPrecededByEffects(String[] reactionTriggers, List<String> pendingEffects) {
         if(reactionTriggers != null)
             for (String trigger: reactionTriggers) {
-                if(pendingEffects.remove(trigger))
+                if(pendingEffects.contains(trigger))
                     return true;
             }
         return false;
     }
-
 
     /**
      *
      * @param effects adds a reaction's effects to the set of effects that can become triggers of subsequent reactions
      * @param pendingEffects set of reactions pending effects
      */
-    private static void addReactionEffects(String[] effects, Set<String> pendingEffects) {
+    private static void addEffects(String[] effects, List<String> pendingEffects) {
         if(effects != null)
             pendingEffects.addAll(Arrays.asList(effects));
     }
@@ -212,6 +213,43 @@ public class Main {
      */
     private static String generateLinkLabel(TraceRecord event){
         return event.getReactor() + DOT_DELIMITER + event.getReactionNumber();
+    }
+
+
+    /**
+     * Adds the transitions to the LF program's graph. One transition node is added if the trace record is a worker time
+     * advancement. If not, there is a causality between reactions, and two nodes are added to the graph.
+     * One transition node with an incident edge's label indicating the executed reactions and a second node with an
+     * incident edge's label indicating the effects produced by the first node's reactions.
+     * @param currentTransition the graph's node representing the highest superdense time
+     * @param pendingReactions a reactions list executed on the time advancement step that is going yo be added to the
+     *                         graph.
+     * @param nextLogicalTimeAdvance the new time advancement step's logical time elapsed
+     * @param currentMicroStep the new time advancement step's micro-step elapsed
+     * @param resultsFromWorkerAdvancing indicates if the time advancement results from a worker advancing time
+     * @return the latest node added to the graph
+     */
+    public static Transition addReactionEffectsTransitions(Transition currentTransition, List<String> effects,
+                                                        List<String> pendingReactions, long nextLogicalTimeAdvance,
+                                                        long currentMicroStep, boolean resultsFromWorkerAdvancing){
+        Transition latestTransition = addTransition(currentTransition, pendingReactions, nextLogicalTimeAdvance,
+                currentMicroStep, resultsFromWorkerAdvancing);
+        currentTransition.setNextTransition(latestTransition);
+        latestTransition.setPreviousTransition(currentTransition);
+        if(resultsFromWorkerAdvancing) {
+            latestTransition.setTriggers(new LinkedList<>());
+            latestTransition.getTriggers().add(TIME_ADVANCE_LABEL_PREFIX);
+        }else
+            latestTransition.setTriggers(pendingReactions);
+
+        if(effects.size() > 0){
+            currentTransition = latestTransition;
+            latestTransition = addTransition(currentTransition, effects, nextLogicalTimeAdvance,
+                    currentMicroStep, resultsFromWorkerAdvancing);
+            currentTransition.setNextTransition(latestTransition);
+            latestTransition.setPreviousTransition(currentTransition);
+        }
+        return latestTransition;
     }
 
     public static void main(String[] args) {
